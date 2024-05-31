a84a871bf5dff324bb462f07b6919f6f65169c3b
Marcelo Rocha <79719807+zombyh@users.noreply.github.com>
2024-05-30 23:10:44 -0300

    Update exemplo_while.py

--- exemplo_while.py
+++ exemplo_while.py
@@ -1,25 +1,13 @@
-"""
-Repetições while (enquanto)
-Executa uma ação enquanto uma condição for verdadeira
-Loop infinito -> Quando um código não tem fim
-"""
-contador = 0
+nome = "Marcelo Rocha"
+indice = 0
+tamanho = len(nome)
+novo_nome = ""
 
-while contador <= 100:
-    contador += 1
+while indice < tamanho:
+    letra = nome[indice]
+    novo_nome += f'|{letra}'
+    indice += 1
+    print(novo_nome)
 
-    if contador == 5:
-        print('Não vou mostrar o 5.')
-        continue
-
-    if contador >= 10 and contador <= 20:
-        print('Não vou mostrar o', contador)
-        continue
-
-    print(contador)
-
-    if contador == 50:
-        break
-
-
-print('Fim')
+novo_nome += '|'
+print(novo_nome)
